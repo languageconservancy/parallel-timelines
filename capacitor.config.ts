21c4a4af9f11c353a31ee86e0ced1ca402e36191
Pete Vieira <pete.vieira@gmail.com>
2025-04-08 11:21:10 -0700

    - Add capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'org.tlc.paralleltimelines',
+  appName: 'Parallel Timelines',
+  webDir: 'dist'
+};
+
+export default config;
